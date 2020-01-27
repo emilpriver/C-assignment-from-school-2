6382086c33be290a2f7c15e93f659a8455238c56
emilpriver <hello@emilpriver.com>
2020-01-27 21:22:21 +0100

    Added Tentamen Class to support the rest of the namespace

---
+++ Tentamen.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tentamensadministration
+{
+    class Tentamen
+    {
+        public string personummer;
+        public int poäng;
+        public string betyg;
+        public Student studenten;
+        public Kurs kursen;
+        public DateTime datum;
+    }
+}
